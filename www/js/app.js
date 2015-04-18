d63042635458ba4393755790bd376600a2ef013c
shaskins <hask0023@algonquinlive.com>
2015-04-18 16:07:14 -0400

    update2

--- www/js/app.js
+++ www/js/app.js
@@ -32,7 +32,16 @@ var app= {
         var addNavBarGridHammer = new Hammer(navBarGrid);
         addNavBarGridHammer.on("tap", app.openGridPage);
         
+    // listeners for the UL list on the grid screen
         
+        var gridList = document.getElementById('gridList');
+        var addGridListHammer = new Hammer(gridList);
+        addGridListHammer.on("tap", app.openModal);
+        
+        // listener for the delete buttons on the grid
+        var deleteImageButton = document.getElementById('deleteImage');
+        var deleteImageButtonHammer = new Hammer(deleteImageButton);
+        deleteImageButtonHammer.on("tap", app.confirmDelete);
 	},
     
     openCamPage: function(ev){
@@ -50,8 +59,8 @@ var app= {
     },
     
     openGridPage: function(ev){
-       // ev.preventDefault();
-      //  ev.target.className = "activetab";
+        
+        // all of the hiding/showing stuff
        
         var activeTab = document.getElementById('gridPageLink');
         var inactiveTab = document.getElementById('camPageLink');
@@ -62,7 +71,45 @@ var app= {
         activePage.className = "active";
         hiddenPage.className = "hidden";
         
+        
+        // show the grid of images
+        
+        
+    },
+    // when you click on a thumbnail, bring up modal with big picture
+    openModal: function(){
+
+    
+    
+    },
+    
+    // when they click the delete button below the thumbnail, pop up confirmation dialogue
+    confirmDelete: function(){
+    navigator.notification.confirm(
+        "Are you sure you want to delete this image?", 
+        function( index ) {
+            switch ( index ) {
+                case 1:
+                    //the first button was pressed
+                    break;
+                    app.deletePhoto;
+                case 2:
+                    // the second button was pressed
+                    break;
+            }
+        },
+        "Delete Image",
+        [ "Yes", "No"]
+    );
+                    
+    
+    },
+    // do AJAX call to delete photo from database, and delete from the interface
+    deletePhoto: function(){
+        
+        
     }
+    
 }
 
 app.init();
